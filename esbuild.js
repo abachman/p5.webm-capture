96720014720214052a5524bcbca4f682f8487c6d
Adam Bachman <13002+abachman@users.noreply.github.com>
2026-09-08 16:15:07 -0400

    chore: add esbuild watch mode

--- esbuild.js
+++ esbuild.js
@@ -1,9 +1,26 @@
-require('esbuild').build({
+const esbuild = require('esbuild');
+
+const buildOptions = {
   entryPoints: ['src/index.ts'],
   platform: "browser",
   bundle: true,
   minify: true,
   sourcemap: true,
-  target: ['es2020', 'chrome58', 'firefox57', 'safari11', 'edge18'],
+  logLevel: 'info',
+  target: ['es2020'],
   outfile: 'dist/p5.webm-capture.js',
-})
+};
+
+if (process.argv.includes('--watch')) {
+  esbuild.context(buildOptions)
+    .then((context) => context.watch())
+    .catch((error) => {
+      console.error(error);
+      process.exit(1);
+    });
+} else {
+  esbuild.build(buildOptions).catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
+}
