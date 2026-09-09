b89acc9eb6991f4fc06ce4634b0675eea51eb430
Adam Bachman <13002+abachman@users.noreply.github.com>
2026-09-09 15:10:45 -0400

    fixed race condition in FrameWrap startup

--- src/capturer.ts
+++ src/capturer.ts
@@ -21,6 +21,8 @@ export default class Capturer {
   el: HTMLCanvasElement | null;
   onComplete: (blob?: Blob) => void;
   manualStop: boolean;
+  startup: undefined | Promise<unknown>
+  verbose: boolean;
 
   constructor() {
     this.active = false;
@@ -31,6 +33,7 @@ export default class Capturer {
     this.frames = 0;
     this.el = null;
     this.onComplete = () => {};
+    this.verbose = false;
   }
 
   enableCapture(settings: CapturerSettings = {}) {
@@ -39,36 +42,52 @@ export default class Capturer {
     const frameCount = settings.frameCount !== undefined ? settings.frameCount : 600;
     this.manualStop = frameCount < 1;
     this.maxFrames = this.manualStop ? Infinity : frameCount;
+    this.verbose = !!settings.verbose;
     this.capture = new FrameWrap({
       framerate: settings.frameRate || 60,
       format: settings.format || "webm",
       quality: settings.quality !== undefined ? settings.quality : 90,
       verbose: !!settings.verbose,
       display: !!settings.display,
+      motionBlurFrames: 1, // don't do motion blur
+      startTime: 0 // always start from the beginning
     });
     this.onComplete = settings.onComplete || (() => {});
   }
 
   // must be safe to call on every frame
-  async startCapture() {
+  startCapture() {
     if (this.active && !this.running && this.capture) {
-      console.log("[webm-capture] started");
+      if (this.verbose) {
+        console.log("[webm-capture] started");
+      }
       this.frames = 0;
       this.running = true;
-      await this.capture.start();
+      this.startup = this.capture.start();
     }
   }
 
   async captureFrame() {
-    if (!this.active || !this.capture) return;
+    if (!this.active || !this.capture) {
+      console.warn('[webm-capture] captureFrame skipped, capturer no longer active or not initialized');
+      return;
+    }
 
     const canvas = this.el || document.querySelector("canvas");
-    if (!canvas) return;
+    if (!canvas) {
+      console.warn('[webm-capture] captureFrame skipped, canvas could not be found');
+      return;
+    }
+
+    // let the CCapture FrameWrap library finish startup before trying to
+    // capture the first frame
+    if (this.startup) await this.startup;
 
     await this.capture.capture(canvas);
-    // this.capture.step()
     this.frames++;
-    console.debug("[webm-capture] captured frame", this.frames, "from", canvas);
+    if (this.verbose) {
+      console.log("[webm-capture] captured frame", this.frames, "from", canvas);
+    }
 
     if (!this.manualStop && this.frames >= this.maxFrames) {
       await this.stopCapture();
@@ -79,10 +98,14 @@ export default class Capturer {
     if (!this.active || !this.capture) return;
     this.active = false;
     this.running = false;
-    console.log("[webm-capture] finished");
     await this.capture.stop();
+    if (this.verbose) {
+      console.log("[webm-capture] finished");
+    }
     const blob = await this.capture.save();
-    console.debug("[webm-capture] saved", blob);
+    if (this.verbose) {
+      console.log("[webm-capture] saved", blob?.size, "bytes");
+    }
     this.onComplete(blob || undefined);
   }
 }
